f90c1e31fa4feff199059c52990696f72b0a33b2
Saramsvi <mscvitech@icloud.com>
2021-09-21 00:34:57 +0300

    Initial Commit

---
+++ SearchBar/SearchBarApp.swift
@@ -0,0 +1,17 @@
+//
+//  SearchBarApp.swift
+//  SearchBar
+//
+//  Created by MSVI on 21.09.21.
+//
+
+import SwiftUI
+
+@main
+struct SearchBarApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
